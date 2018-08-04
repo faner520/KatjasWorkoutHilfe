a1c5c1d2fec14d5fdf0b72c49e0b99da0e603460
Robert Fuchs <robertfuchs97@gmail.com>
2018-08-04 14:01:36 +0200

    add logic to start animation when element is in the viewport

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,4 +1,27 @@
-$(document).ready(function() {
-    $(".left-unordered-list").addClass("animation-end");
-    $(".right-unordered-list").addClass("animation-end");
+function isElementInViewport (el) {
+
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+$(document).ready(() =>  {
+    const boxLevel3 = $(".box-level-3");
+
+    $(window).scroll(() => {
+        if (isElementInViewport(boxLevel3)) {
+            $(".left-unordered-list").addClass("animation-end");
+            $(".right-unordered-list").addClass("animation-end");
+        }
+    });
 });
