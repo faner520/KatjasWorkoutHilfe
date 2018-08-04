1b4ee91837df926985188cea39d743aa0a795ba5
Robert Fuchs <robertfuchs97@gmail.com>
2018-08-04 13:25:13 +0200

    animate muskelgroups to slide and fade in from the side

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-
-    });
-
-
+    $(".left-unordered-list").addClass("animation-end");
+    $(".right-unordered-list").addClass("animation-end");
 });
